e20f1f3d925f68749f59b3dac1f808188588cad4
Ananyasinha7 <anany08a@gmail.com>
2025-08-09 20:08:10 +0530

    updates

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,21 +1,18 @@
 import pandas as pd
-import chromadb
+from langchain_community.vectorstores import FAISS
 import uuid
 
 class Portfolio:
     def __init__(self,file_path='app/resource/my_portfolio.csv'):
         self.file_path=file_path
         self.data=pd.read_csv(file_path)
-        self.chroma_client=chromadb.PersistentClient('vectorstore')
-        self.collection=self.chroma_client.get_or_create_collection(name='portfolio')
+        self.vectorstore=FAISS.from_documents(self.data['Techstack'], self.data['Links'])
 
     def load_portfolio(self):
-        if not self.collection.count():
-            for _,row in self.data.iterrows():
-                self.collection.add(documents=row['Techstack'],
-                                    metadatas={'Links':row['Links']},
-                                    ids=[str(uuid.uuid4())])
+        self.vectorstore.add_documents(self.data['Techstack'], self.data['Links'])
+
+                                   
                 
 
     def query_links(self,skills):
-        return self.collection.query(query_texts=skills,n_results=2).get('metadatas',[])
+        return self.vectorstore.query(query_texts=skills,n_results=2).get('metadatas',[])
--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,9 @@
 import re
 
 def clean_text(text):
-    text=re.sub(r'<[^>]*?>','',text) #remove html tags
-    text=re.sub(r'https?://(?:[a-zA-Z]|[0-9]|[$-_@,&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',text) #remove URLs
-    text=re.sub(r'[^a-zA-Z0-9\s]',' ',text) #replace special characters with space
-    text=re.sub(r'\s+',' ',text) #replace multiple spaces with single space
+    text=re.sub(r'<[^>]*?>','',text) #removed html tags
+    text=re.sub(r'https?://(?:[a-zA-Z]|[0-9]|[$-_@,&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+','',text) #removed URLs
+    text=re.sub(r'[^a-zA-Z0-9\s]',' ',text) #removed special characters
+    text=re.sub(r'\s+',' ',text) #removed multiple spaces
     text=text.strip()
     return text
